3ffcce83509c5b684ac9e3fcb7a046b6a49eee3e
Mateus de Oliveira Sposo <mat.sposo@gmail.com>
2024-04-29 23:10:51 -0300

    correção de problemas e implementação de interface e liberação de memória

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAX 50
+#define TAM 16811
 
 typedef unsigned char bool;
 static const bool false = 0;
@@ -22,6 +23,7 @@ typedef struct _cidade{
 typedef struct _hash{
     tcidade *cidades;
     int tamanho; // 16811 por ser primo
+    int atual;
 }thash;
 
 thash* criarHash(int tamanho){
@@ -34,56 +36,56 @@ thash* criarHash(int tamanho){
     return hash;
 }
 
-int h(int codigo_ibge, int tamanho){
+int h1(int codigo_ibge, int tamanho){
     return (codigo_ibge) % tamanho;
 }
 
-void imprimeInforamacoes(tcidade cidade){
+int h2(int codigo_ibge, int tamanho){
+    return 1 + (codigo_ibge) % (tamanho - 1);
+}
+
+void imprimeInformacoes(tcidade cidade){
     printf("Codigo IBGE: %d\n", cidade.codigo_ibge);
-    printf("Nome: %s\n", cidade.nome);
+    printf("Nome: \"%s\"\n", cidade.nome);
     printf("Latitude: %lf\n", cidade.latitude);
     printf("Longitude: %lf\n", cidade.longitude);
     printf("Capital: %d\n", cidade.capital);
     printf("Codigo UF: %d\n", cidade.codigo_uf);
     printf("Siafi ID: %d\n", cidade.siafi_id);
     printf("DDD: %d\n", cidade.ddd);
-    printf("Fuso Horario: %s\n", cidade.fuso_horario);
+    printf("Fuso Horario: \"%s\"\n", cidade.fuso_horario);
 }
 
 void insereCidade(thash *hash, tcidade cidade){
     int i = 0;
     int pos; 
     while(true){
-        pos = (h(cidade.codigo_ibge, hash->tamanho) + i * h(cidade.codigo_ibge, hash->tamanho))% hash->tamanho;
+        pos = (h1(cidade.codigo_ibge, hash->tamanho) + i * h2(cidade.codigo_ibge, hash->tamanho)) % hash->tamanho;
         if(hash->cidades[pos].codigo_ibge == 0){
             hash->cidades[pos] = cidade;
-            break;
-        }
-        i++; //testar depois
-    }
-    if(strcmp(cidade.nome, "Jales") == 0){
-        imprimeInforamacoes(hash->cidades[pos]);
-        printf("%d\n",pos);
-    }
-}
-
-void buscaCidade(thash* hash, int codigo_ibge){
-    int i = 0;
-    int pos = h(codigo_ibge, hash->tamanho);
-    int inicio = pos;
-    while(true){
-        if(hash->cidades[pos].codigo_ibge == codigo_ibge){
-            printf("Cidade encontrada! %d\n", pos);
-            imprimeInforamacoes(hash->cidades[pos]);
-            break;
-        }
-        if(pos == inicio){
-            printf("Cidade nao encontrada!\n");
-            printf("%d\n", pos);
+            hash->atual += 1;
             break;
         }
         i++;
-        pos = (h(codigo_ibge, hash->tamanho) + i * h(codigo_ibge, hash->tamanho))% hash->tamanho;
+    }
+}
+
+void buscaIBGE(thash* hash, int codigo_ibge){
+    int i = 0;
+    int pos = h1(codigo_ibge, hash->tamanho);
+    while(true){
+        if(hash->cidades[pos].codigo_ibge == codigo_ibge){
+            printf("Cidade encontrada!\n");
+            printf("--------------------------\n");
+            imprimeInformacoes(hash->cidades[pos]);
+            break;
+        }
+        if(i > hash->tamanho){
+            printf("Cidade nao encontrada!\n");
+            break;
+        }
+        i++;
+        pos = (h1(codigo_ibge, hash->tamanho) + i * h2(codigo_ibge, hash->tamanho)) % hash->tamanho;
     }
 }
 
@@ -122,13 +124,58 @@ void lerArquivo(FILE* arquivo, thash* hash){
     }
 }
 
-int main(){
-    thash *hash = criarHash(16811);
-    FILE* arquivo = fopen("../dados/municipios.json", "r");
+void liberaHash(thash* hash){
+    free(hash->cidades);
+    free(hash);
+}
 
+void interface(thash *hash){
+    int opcao = 0;
+    while(opcao != 4){
+        printf("--------------------------\n");
+        printf("1 - Busca por Codigo IBGE\n");
+        printf("2 - Busca n Vizinhos\n");
+        printf("3 - Busca por Nome\n");
+        printf("4 - Sair\n");
+        printf("--------------------------\n");
+        printf("Digite a opcao desejada: ");
+        scanf("%d", &opcao);
+        printf("--------------------------\n");
+        int codigo_ibge;
+        switch(opcao){
+            case 1:
+                printf("Digite o codigo IBGE: ");
+                scanf("%d", &codigo_ibge);
+                buscaIBGE(hash, codigo_ibge);
+                break;
+            case 2:
+                printf("Digite o codigo IBGE: ");
+                scanf("%d", &codigo_ibge);
+                //buscaVizinhos(hash, codigo_ibge);
+                break;
+            case 3:
+                printf("Digite o nome da cidade: ");
+                char nome[MAX];
+                scanf("%s", nome);
+                //buscaNome(hash, nome);
+                break;
+            case 4:
+                liberaHash(hash);
+                break;
+            default:
+                printf("Opcao invalida!\n");
+                break;
+        }
+    }
+}
+
+
+int main(){
+    thash *hash = criarHash(TAM);
+    FILE* arquivo = fopen("../dados/municipios.json", "r");
     lerArquivo(arquivo, hash);
 
-    buscaCidade(hash, 3524808);
+    interface(hash);
 
     fclose(arquivo);
     return EXIT_SUCCESS;
